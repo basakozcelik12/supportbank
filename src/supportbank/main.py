17a43b4eb7ebd76332aa939959e4fc1facf891a0
Basak Ozcelik <basak.ozcelik@softwire.com>
2026-03-11 17:04:54 +0000

    first commit
    Add accounts and transactions

---
+++ src/supportbank/main.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+import csv
+
+class Account:
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+        self.transactions = [] 
+
+    def payDebt(self, payment_to, date, amount, narrative):
+        self.balance -= amount
+        self.transactions.append(f"{self.name} paid to {payment_to} amount {amount} for {narrative} at {date}")
+
+    def getDebt(self, payment_from, date, amount, narrative):
+        self.balance += amount
+        self.transactions.append(f"{payment_from} paid to {self.name} amount {amount} for {narrative} at {date}")
+        
+accounts = {}
+file_path = "src/supportbank/Transactions2014.csv"
+
+with open(file_path, newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        payer = row['From']
+        receiver = row['To']
+        amount = float(row['Amount'])
+        date = row['Date']
+        narrative = row['Narrative']
+
+        if payer not in accounts:
+            accounts[payer] = Account(payer)
+        if receiver not in accounts:
+            accounts[receiver] = Account(receiver)
+
+        accounts[payer].payDebt(receiver, date, amount, narrative)
+        accounts[receiver].getDebt(payer, date, amount, narrative)
+
+
+
